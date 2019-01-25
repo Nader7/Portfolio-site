1657d3fb3fee5830102125baf8a933b81011b9e9
Nader7 <mnader767@gmail.com>
2019-01-25 20:55:31 +1100

    Added links to project slides

--- app/assets/javascripts/page_scroll.js
+++ app/assets/javascripts/page_scroll.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
+		anchors:['title','projects','skills'],
+		lockAnchors:true,
 		autoScrolling:true,
 		navigation: true,
 		loopBottom: true,
@@ -10,4 +12,14 @@ $(document).ready(function() {
 
 	//methods
 	$.fn.fullpage.setAllowScrolling(true);
+
+	$('#bionic').click(function(){
+    	fullpage_api.moveTo('projects',1);
+	});
+	$('#energy').click(function(){
+    	fullpage_api.moveTo('projects',2);
+	});
+	$('#game').click(function(){
+    	fullpage_api.moveTo('projects',3);
+	});
 });
