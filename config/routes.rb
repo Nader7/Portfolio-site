f8010ea64757d1a53efaf7260eb25572367c79d4
Nader7 <mnader767@gmail.com>
2019-01-27 14:02:34 +1100

    Posts controller/new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "home#show", page: "home"
   get "home/:page" => "home#show"
   get 'projects' => "home#projects" #links to js.erb file
+  resources :posts
 end
