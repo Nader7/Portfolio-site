de3f48690313a1d12255f7361547bfb785aed663
Nader7 <mnader767@gmail.com>
2019-01-29 17:18:01 +1100

    added show and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
 		@post = Post.new(post_params)
 		@post.save
-		redirect_to @post
-	end
-
-	def index #May not be needed, defined incase url is entered
-		render 'home/projects.js.erb'
+		render partial: 'show', locals: {post: @post}, remote:true
 	end
 
 	private
