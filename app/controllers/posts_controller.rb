222011b234c504175f114287f651f0cb7060917c
Nader7 <mnader767@gmail.com>
2019-01-29 23:23:48 +1100

    update and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,38 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
+	end
+	
+	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-		render partial: 'show', locals: {post: @post}, remote:true
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+		#render partial: 'show', locals: {post: @post}, remote:true
+	end
+	
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 	private
