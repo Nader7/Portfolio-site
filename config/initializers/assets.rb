80b72654bc54f0bffa34de9ff5859a72eb3a0e1f
Nader7 <mnader767@gmail.com>
2019-02-19 23:44:50 +1100

    updated puma/added assets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( fullpage.extensions.min.js fullpage.min.css )
+Rails.application.config.assets.precompile += %w( page_scroll.js )
+Rails.application.config.assets.precompile += %w( projects.js.erb )
