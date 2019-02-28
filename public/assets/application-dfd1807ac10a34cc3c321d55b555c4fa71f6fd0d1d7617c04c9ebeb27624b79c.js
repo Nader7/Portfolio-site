d4df6bcce790014b8262788296225c823453f4a5
Nader7 <mnader767@gmail.com>
2019-02-28 13:32:17 +1100

    added project backgrounds

--- public/assets/application-dfd1807ac10a34cc3c321d55b555c4fa71f6fd0d1d7617c04c9ebeb27624b79c.js
+++ public/assets/application-dfd1807ac10a34cc3c321d55b555c4fa71f6fd0d1d7617c04c9ebeb27624b79c.js
@@ -17718,47 +17718,6 @@ Copyright © 2018 Basecamp, LLC
 
 
 }).call(this);
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		anchors:['title','projects','skills'],
-		lockAnchors: true,
-		autoScrolling: true,
-		loopBottom: true,
-		controlArrows: false,
-		verticalCentered: true,
-		scrollingSpeed: 850,
-		navigation: true,
-		navigationTooltips: ['Title', 'Bionic Sim', 'Energy System', 'Zorkish', 'Skills'],
-		showActiveTooltip: true,
-		scrollOverflow: true,
-		licenseKey: 'AA5B0793-878946C9-AD56A913-DD44D5FC',
-		afterRender: function () { //defined in this block for compatibility with fullpage
-			$('.lightbox_image').magnificPopup({type:'iframe'});
-		}
-	});
-	
-	$.fn.fullpage.setAllowScrolling(true);
-});
-
-$(document).on('click', '#project_back', function(){ //for the project layout back button
-	var slide = $('.fp-section.active').find('.slide.active');
-	$.fn.fullpage.moveSlideLeft();
-    setTimeout(function(){
-    	slide.remove();
-    },700);
-});
-
-$(document).on('click', '#back_title', function(){
-	$.fn.fullpage.moveSlideLeft();
-});
-
-$(document).on('click', '#features', function(){
-	$.fn.fullpage.moveSlideRight();
-});
-
-$(document).on('click', '#title_down', function(){
-	$.fn.fullpage.moveSectionDown();
-});
 /*!
  * fullPage 3.0.4 - Extensions 0.1.7
  * https://github.com/alvarotrigo/fullPage.js
@@ -17789,6 +17748,39 @@ function(t,i){t.fp_scrolloverflow=function(){t.IScroll||(IScroll=module.exports)
 
 
 }).call(this);
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors:['title','projects','skills'],
+		lockAnchors: true,
+		autoScrolling: true,
+		loopBottom: true,
+		controlArrows: false,
+		verticalCentered: true,
+		scrollingSpeed: 850,
+		navigation: true,
+		navigationTooltips: ['Title', 'Bionic Sim', 'Energy System', 'Zorkish', 'Skills'],
+		showActiveTooltip: true,
+		scrollOverflow: true,
+		licenseKey: 'AA5B0793-878946C9-AD56A913-DD44D5FC',
+		afterRender: function () { //defined in this block for compatibility with fullpage
+			$('.lightbox_image').magnificPopup({type:'iframe'});
+		}
+	});
+	
+	$.fn.fullpage.setAllowScrolling(true);
+});
+
+$(document).on('click', '#back_title', function(){
+	$.fn.fullpage.moveSlideLeft();
+});
+
+$(document).on('click', '#features', function(){
+	$.fn.fullpage.moveSlideRight();
+});
+
+$(document).on('click', '#title_down', function(){
+	$.fn.fullpage.moveSectionDown();
+});
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
